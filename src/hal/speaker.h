daf30aac134da859cc91dcdfa1546128f65bd58b
hmightypirate <hcerezoc@gmail.com>
2020-11-28 18:39:53 +0100

    Adding speaker funcitons from MEIGA

--- src/hal/speaker.h
+++ src/hal/speaker.h
@@ -2,6 +2,8 @@
 #define __SPEAKER_H
 
 
+#include <libopencm3/stm32/timer.h>
+#include "setup.h"
 
 void speaker_on(float hz);  // Used by mmlib (music)
 void speaker_off(void);   // Used by mmlib (music)
