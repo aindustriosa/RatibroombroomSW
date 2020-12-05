ccc1de3aafa23f4c0e6ce1e6736358d023b31031
Jorge Muñoz <punkto@gmail.com>
2020-12-05 21:33:10 +0100

    WIP #7: complete serial.h and start serial.c

--- src/hal/serial.c
+++ src/hal/serial.c
@@ -1,8 +1,27 @@
+/* Serial interface implementation.
+ * We are using USART1, which is configured in setup.c
+ * We also use 2 DMA channels (DMA controller 2, stream 7 for TX).
+ */
+
 #include "serial.h"
 
-
-static char receive_buffer[RECEIVE_BUFFER_SIZE];
-
+/* Reveived data from serial buffer:
+ * This buffer is used by the USART RX DMA channel to store the incoming bytes.
+ * Note that this buffer can be use by higher levels directly.
+ */
+static char receive_buffer[RECEIVE_BUFFER_SIZE] = {0};
+/* Serial TX mutex 
+ * This mutex is used to avoid more than one source writting at the same time.
+ * The mutex is adquired by the user using serial_acquire_transfer_lock() and is
+ * released by the USART TX DMA channel ISR. 
+ */
+static mutex_t _send_lock;
+/* Command received flag.
+ * This flag is set to true by the USART ISR if a '\0' is found, so the arrived data can be 
+ * processed as a new command in upper layers.
+ * The upper layers must set it again to false once processed.
+ */
+static volatile bool received = false;
 
 /**
  * @brief Try to acquire the serial transfer lock.
@@ -11,41 +30,59 @@ static char receive_buffer[RECEIVE_BUFFER_SIZE];
  */
 bool serial_acquire_transfer_lock(void)
 {
-  //FIXME: mock
-  return false;
-
+	return (bool)mutex_trylock(&_send_lock);
 }
-
 
 /**
  * @brief Send data through serial.
  *
  * DMA is configured to read from `data` a number `size` of bytes. It then
- * writes all those bytes to USART3 (Bluetooth).
+ * writes all those bytes to USART1.
  *
  * An interruption is generated when the transfer is complete.
+ * 
+ * Examples of DMA configuration for F4 in
+ * https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/f4/stm32f4-discovery/dac-dma/dac-dma.c
+ * https://github.com/froj/IMU/blob/master/sleephase.c
  *
  * @param[in] data Data to send.
  * @param[in] size Size (number of bytes) to send.
  */
 void serial_send(char *data, int size)
 {
-  //FIXME: mock
-  
+	dma_stream_reset(DMA2, DMA_STREAM7);
+
+	dma_enable_memory_increment_mode(DMA2, DMA_STREAM7);
+	dma_set_peripheral_size(DMA2, DMA_STREAM7, DMA_SxCR_PSIZE_8BIT);
+	dma_set_memory_size(DMA2, DMA_STREAM7, DMA_SxCR_MSIZE_8BIT);
+	dma_set_priority(DMA2, DMA_STREAM7, DMA_SxCR_PL_VERY_HIGH);
+	dma_set_transfer_mode(DMA2, DMA_STREAM7, DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+
+	dma_set_peripheral_address(DMA2, DMA_STREAM7, (uint32_t)&USART1_DR);
+	dma_set_memory_address(DMA2, DMA_STREAM7, (uint32_t)data);
+	dma_set_number_of_data(DMA2, DMA_STREAM7, size);
+
+	dma_enable_transfer_complete_interrupt(DMA2, DMA_STREAM7);
+	dma_channel_select(DMA2, DMA_STREAM7, DMA_SxCR_CHSEL_4);
+	dma_enable_stream(DMA2, DMA_STREAM7);
+	usart_enable_tx_dma(USART1);
 }
 
 bool get_received_command_flag(void)
 {
-  // FIXME: mock
-  return false;
+	// FIXME: mock
+	return false;
 }
 
 void set_received_command_flag(bool value)
 {
-  // FIXME: mock
+	// FIXME: mock
 }
 
 char *get_received_serial_buffer(void)
 {
 	return receive_buffer;
 }
+
+// USART IRQ example
+// https://github.com/libopencm3/libopencm3-examples/tree/master/examples/stm32/f4/stm32f4-discovery/usart_irq
--- src/hal/serial.h
+++ src/hal/serial.h
@@ -1,16 +1,61 @@
+/* Serial Port Interface.
+ * This serial port is used for user interaction through commands and logging. 
+ */
+
 #ifndef __SERIAL_H
 #define __SERIAL_H
 
 #include <libopencm3/cm3/sync.h>
 #include <libopencm3/stm32/usart.h>
-#include "mmlib/logging.h"
+#include <libopencm3/stm32/dma.h>
+//#include "mmlib/logging.h"   // hal should not depend on mmlib!!
 
-#define RECEIVE_BUFFER_SIZE 256   // Used by mmlib (command) // FIXME: bulebule specs
+#define RECEIVE_BUFFER_SIZE 256   // Used by mmlib (command) // FIXME: move to bulebule specs?
 
-bool get_received_command_flag(void); // Used by mmlib (command)
-void set_received_command_flag(bool value); // Used by mmlib (command)
-bool serial_acquire_transfer_lock(void);  // Used by mmlib
-void serial_send(char *data, int size);  // Used by mmlib
-char *get_received_serial_buffer(void); // Used by mmlib (command)
+/**
+ * @brief Checks if a command has arrived (command_flag == true).
+ * 
+ *  Used by mmlib (command)
+ *
+ *@return True if the buffer contains a command to be executed.
+ */
+bool get_received_command_flag(void);
+
+/**
+ * @brief Sets the value of command_flag (true if a command waits to be executed).
+ * 
+ *  Used by mmlib (command)
+ *
+ * @param[in] bool New flag value.
+ */
+void set_received_command_flag(bool value);
+
+/**
+ * @brief TODO
+ * 
+ *  Used by mmlib
+ *
+ *@return TODO
+ */
+bool serial_acquire_transfer_lock(void);
+
+/**
+ * @brief TODO
+ * 
+ *  Used by mmlib
+ *
+ * @param[in] data Data to send.
+ * @param[in] size Size (number of bytes) to send.
+ */
+void serial_send(char *data, int size);
+
+/**
+ * @brief Returns the serial buffer address where the incoming bytes are stored.
+ * 
+ *  Used by mmlib (command)
+ *
+ *@return a pointer to the char array
+ */
+char *get_received_serial_buffer(void);
 
 #endif /* __SERIAL_H */
