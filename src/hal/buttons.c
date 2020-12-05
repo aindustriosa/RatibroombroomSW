0bbcece732c9ef15e5986f26f20fcc705784f68d
eLeDe <little_elede@hotmail.com>
2020-12-05 16:53:39 +0100

    feat: add code to button mock

--- src/hal/buttons.c
+++ src/hal/buttons.c
@@ -5,6 +5,21 @@
  */
 bool button_read_user(void)
 {
-  // FIXME: mock
-  return false;
+  return button_read_right();
+}
+
+/**
+ * @brief Function to read SW1 - Left button.
+ */
+bool button_read_left(void)
+{
+  return (bool)(gpio_get(GPIOB, GPIO12));
+}
+
+/**
+ * @brief Function to read SW2 - Right button.
+ */
+bool button_read_right(void)
+{
+  return (bool)(gpio_get(GPIOC, GPIO13));
 }
