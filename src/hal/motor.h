90a3481a2b1dc025e6e8d4805a888e483ef6d40c
eLeDe <little_elede@hotmail.com>
2020-12-12 19:40:15 +0100

    feat: code in motor functions

--- src/hal/motor.h
+++ src/hal/motor.h
@@ -6,6 +6,7 @@
 uint32_t motor_driver_saturation(void); // Used by mmlib (control)
 void reset_motor_driver_saturation(void);  // Used by mmlib (control)
 void drive_break(void); // Used by mmlib (move)
+void drive_brake(void); // drive_break() rename
 void drive_off(void); // Used by mmlib (control)
 void power_left(int32_t power); // Used by mmlib (control)
 void power_right(int32_t power); // Used by mmlib (control)
